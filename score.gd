61b8443cddc9573ace853afc95aa8e6845a2cde7
Oskar van Velden <oskar@rakso.nl>
2020-07-05 22:22:17 +0200

    Tweak score animation

--- score.gd
+++ score.gd
@@ -5,9 +5,12 @@ onready var shoutLabelContainer = $CenterContainer/Control
 onready var shoutLabel = $CenterContainer/Control/ScoreShout
 onready var shoutTween = $CenterContainer/Control/Tween
 
+var playerRef : Node2D
+
 func _ready():
 	hide()
 	shoutLabelContainer.hide()
+	playerRef = get_tree().get_nodes_in_group('player').front()
 
 func reset():
 	show()
@@ -20,8 +23,8 @@ func set_counter(text):
 	shoutLabelContainer.show()
 	shoutLabel.text = str(text)
 	
-	shoutTween.interpolate_property(shoutLabelContainer, "rect_scale", shoutLabelContainer.rect_scale, Vector2(5.0, 5.0), 1.0, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
-	shoutTween.interpolate_property(shoutLabelContainer, "modulate", Color(1.0, 1.0, 1.0, 1.0), Color(1.0, 1.0, 1.0, 0.0), 1.0, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	shoutTween.interpolate_property(shoutLabelContainer, "rect_scale", shoutLabelContainer.rect_scale, Vector2(2.0, 2.0), 0.6, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	shoutTween.interpolate_property(shoutLabelContainer, "modulate", Color(1.0, 1.0, 1.0, 1.0), Color(1.0, 1.0, 1.0, 0.0), 0.6, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
 	shoutTween.start()
 	
 	yield(shoutTween, 'tween_completed')
